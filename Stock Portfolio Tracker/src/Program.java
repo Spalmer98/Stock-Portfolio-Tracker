1c3c9596ccbf47d2181f254dafd8becc07c97b7c
Spencer Palmer <monkies989@gmail.com>
2025-03-12 16:44:35 -0600

    Began creating the structure of the stock program

---
+++ Stock Portfolio Tracker/src/Program.java
@@ -0,0 +1,8 @@
+public class Program 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        ClearScreen.clearConsole();
+        System.out.println("Hello, World!");
+    }
+}
